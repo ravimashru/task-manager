428a9e02a79611b6257496e3a0328650dc1ad694
Ravi Mashru <mashru.ravi@gmail.com>
2020-06-15 08:31:06 +0530

    Initial commit

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,46 @@
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v1 as uuid_v1 } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TasksService {
+
+  private tasks: Task[] = [];
+
+  public getAllTasks(): Task[] {
+    return this.tasks.slice();
+  }
+
+  public createTask(createTaskDto: CreateTaskDto): Task {
+
+    const { title, description } = createTaskDto;
+
+    const task: Task = {
+      id: uuid_v1(),
+      title,
+      description,
+      status: TaskStatus.OPEN
+    };
+    this.tasks.push(task);
+    return task;
+  }
+
+  public getTaskById(id: string): Task {
+    const task = this.tasks.find(e => e.id === id);
+    if (!task) {
+      throw new NotFoundException();
+    }
+    return task;
+  }
+
+  public deleteTask(id: string): void {
+    this.tasks = this.tasks.filter(e => e.id !== id);
+  }
+
+  public updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
+    return task;
+  }
+}
